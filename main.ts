7f60d61c2070efb21fdfdcc6439a006abf0969e5
sh4869 <nobuk4869@gmail.com>
2020-09-07 17:07:05 +0900

    update scripts

--- main.ts
+++ main.ts
@@ -11,63 +11,76 @@ type PkgDataInfo = { [key: string]: PkgData };
 
 const sleep: (number: number) => Promise<void> = msec => new Promise<void>(resolve => setTimeout(resolve, msec));
 
-const getPackageInfo = async (opt: pkginfo.Options, count: number = 0): Promise<PkgDataInfo> => {
+const getPackageInfo = async (opt: pkginfo.Options, sleeptime, count = 0): Promise<PkgDataInfo> => {
   try {
-    await sleep(Math.random() * 10000);
+    await sleep(sleeptime * 300);
     const func = util.promisify(pkginfo);
     const result = await func(opt)
       .then(v => v.data)
-      .catch(v => {});
+      .catch(e => {
+        console.log(e.toString());
+        throw e;
+      });
     return result;
   } catch {
     if (count > 3) {
+      console.log(`failed: ${opt.packages[0]},${opt.packages[1]},...`);
       return {};
     }
-    console.log(`retry: ${opt.packages[0]}...`);
+    console.log(`retry: ${opt.packages[0]},${opt.packages[1]},...`);
     await sleep(1000);
-    return getPackageInfo(opt, count + 1);
+    return getPackageInfo(opt, sleeptime, count + 1);
   }
 };
 
-const INVALID_PATH_REGEX = /[\u0000-\u0020\u0100-\uffff]/;
-
-const PERNUM = 100;
+const PERNUM = 50;
 const MAX = 20000;
 const main = async () => {
   const v = names as string[];
   const separte = Math.floor(v.length / MAX);
   console.log(`package count: ${v.length},file count: ${separte}`);
-  for (let count = 0; count < separte; count++) {
-    console.log(`start ${count} time...`);
+  for (let count = 0; count <= separte; count++) {
+    console.log(`start ${count}/${separte}`);
     const start = count * MAX;
-    const goal = (count + 1) * MAX - 1;
-    console.log(start,goal)
+    const goal = (count + 1) * MAX - 1 > v.length ? v.length - 1 : (count + 1) * MAX - 1;
+    console.log(start, goal);
     const arr: Promise<PkgDataInfo>[] = [];
+    let sleeptime = 0;
     for (let i = start; i < goal; i += PERNUM) {
       const option: pkginfo.Options = {
         packages: v.slice(i, i + PERNUM > goal ? goal : i + PERNUM).map(x => encodeURIComponent(x))
       };
-      arr.push(getPackageInfo(option));
+      arr.push(
+        getPackageInfo(option, sleeptime).then(v => {
+          console.log(`end: ${i}`);
+          return v;
+        })
+      );
+      sleeptime++;
     }
+    console.log("start all promises...");
     await Promise.all(arr)
       .then(v => {
         console.log(`fetch all library... ${v.length}`);
-        const result = v.filter(x => x !== undefined && x !== null)
-          .map(x => Array.from(Object.keys(x)).filter(name => x[name].versions) // package name array
-              .map(name =>({
-                    name: name,
-                    versions: Array.from(Object.keys(x[name].versions)).map(version => ({
-                      version: version,
-                      dep: x[name].versions[version].dependencies
-                    }))
-                  })
-              ))
+        const result = v
+          .filter(x => x !== undefined && x !== null)
+          .map(x =>
+            Array.from(Object.keys(x))
+              .filter(name => x[name].versions) // package name array
+              .map(name => ({
+                name: name,
+                versions: Array.from(Object.keys(x[name].versions)).map(version => ({
+                  version: version,
+                  dep: x[name].versions[version].dependencies
+                }))
+              }))
+          )
           .reduce((p, v) => p.concat(v), []);
         console.log("writing file...");
         writeFileSync(`result/${count}.json`, JSON.stringify(result, null, 4));
       })
       .catch(v => {
-        console.log(v);
+        console.log(`error: ${v}`);
       });
   }
 };
