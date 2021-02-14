6b778831067ba13a2371bebfea72fca1af196a54
sh4869 <nobuk4869@gmail.com>
2021-02-14 14:55:36 +0900

    update script using node-fetch

--- main.ts
+++ main.ts
@@ -1,84 +1,82 @@
-import pkginfo = require("npm-registry-package-info");
 import packagejson = require("package-json");
 import { readFileSync, writeFileSync } from "fs";
-import util = require("util");
-
+import fetch from "node-fetch";
 
 interface PkgData {
   versions: { [key: string]: packagejson.AbbreviatedVersion };
+  name: string;
 }
-type PkgDataInfo = { [key: string]: PkgData };
+interface Data {
+  name: string;
+  data: PkgData;
+}
 
 const sleep: (number: number) => Promise<void> = msec => new Promise<void>(resolve => setTimeout(resolve, msec));
 
-const getPackageInfo = async (opt: pkginfo.Options, sleeptime, count = 0): Promise<PkgDataInfo> => {
+const url = (name: string): string => `https://registry.npmjs.org/${encodeURIComponent(name)}`;
+const fetchPkg = async (name: string): Promise<Data> => {
   try {
-    await sleep(sleeptime * 50);
-    const func = util.promisify(pkginfo);
-    const s = sleep(60000);
-    const result = await Promise.race([s, func(opt).then(v => v.data)]);
-    if (result == undefined) {
-      throw Error("timeout!");
-    }
-    return result;
+    return await fetch(url(name))
+      .then(v => v.json())
+      .then(v => ({ name: name, data: v } as Data));
   } catch {
-    if (count > 3) {
-      console.log(`failed: ${opt.packages[0]},${opt.packages[1]},...`);
-      return {};
-    }
-    console.log(`retry: ${opt.packages[0]},${opt.packages[1]},...`);
-    await sleep(500);
-    return getPackageInfo(opt, sleeptime, count + 1);
+    await sleep(300);
+    console.log(`retry: ${name}`);
+    return fetchPkg(name);
   }
 };
 
-const PERNUM = 25;
+const convertData = (data: Data) => {
+  try {
+    const pkgData = data.data;
+    return {
+      name: pkgData.name,
+      versions: Array.from(Object.keys(pkgData.versions)).map(version => ({
+        version: version,
+        dep: pkgData.versions[version].dependencies
+      }))
+    };
+  } catch {
+    return data.name;
+  }
+};
+
 const MAX = 20000;
-const main = async () => {
+const main = async (): Promise<void> => {
   // 引数からファイル番号を受け取れるように
-  const start = process.argv.length > 2 ? Number(process.argv[2]) : 0
-  const v = JSON.parse(readFileSync("all-the-package-names/names.json").toString()) as string[];
-  const separte = Math.floor(v.length / MAX);
-  console.log(`package count: ${v.length},file count: ${separte}`);
+  const start = process.argv.length > 2 ? Number(process.argv[2]) : 0;
+  const names = JSON.parse(readFileSync("all-the-package-names/names.json").toString()) as string[];
+  const separte = Math.floor(names.length / MAX);
+  console.log(`package count: ${names.length},file count: ${separte}`);
   for (let count = start; count <= separte; count++) {
     console.log(`start ${count}/${separte}`);
     const start = count * MAX;
-    const goal = (count + 1) * MAX - 1 > v.length ? v.length - 1 : (count + 1) * MAX - 1;
+    const goal = (count + 1) * MAX - 1 > names.length ? names.length - 1 : (count + 1) * MAX - 1;
     console.log(start, goal);
-    const arr: Promise<PkgDataInfo>[] = [];
+    const arr: Promise<Data>[] = [];
     let sleeptime = 0;
-    for (let i = start; i < goal; i += PERNUM) {
-      const option: pkginfo.Options = {
-        packages: v.slice(i, i + PERNUM > goal ? goal : i + PERNUM).map(x => encodeURIComponent(x))
-      };
+    let complete = 0;
+    for (let i = start; i < goal; i++) {
       arr.push(
-        getPackageInfo(option, sleeptime).then(v => {
-          console.log(`end: ${i}`);
-          return v;
-        })
+        sleep(sleeptime)
+          .then(_ => fetchPkg(names[i]))
+          .then(v => {
+            complete += 1;
+            if (complete % 1000 == 0) console.log(`end ${complete}/${goal - start}`);
+            return v;
+          })
       );
-      sleeptime++;
+      sleeptime += 5;
     }
     console.log("start all promises...");
     await Promise.all(arr)
       .then(v => {
-        console.log(`fetch all library... ${v.length}`);
-        const result = v
-          .filter(x => x !== undefined && x !== null)
-          .map(x =>
-            Array.from(Object.keys(x))
-              .filter(name => x[name].versions) // package name array
-              .map(name => ({
-                name: name,
-                versions: Array.from(Object.keys(x[name].versions)).map(version => ({
-                  version: version,
-                  dep: x[name].versions[version].dependencies
-                }))
-              }))
-          )
-          .reduce((p, v) => p.concat(v), []);
+        const result = v.map(x => convertData(x));
+        const errors = result.filter(v => typeof v == "string");
+        const ok = result.filter(v => typeof v != "string");
+        console.log(`error on request ${errors.length} package: ${errors.join(",")}`);
         console.log("writing file...");
-        writeFileSync(`result/${count}.json`, JSON.stringify(result, null, 4));
+        writeFileSync(`result/${count}.json`, JSON.stringify(ok));
       })
       .catch(v => {
         console.log(`error: ${v}`);
