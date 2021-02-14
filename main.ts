157136cbfc4bb1be14bd0e9731263a46c0a143b7
sh4869 <nobuk4869@gmail.com>
2021-02-14 22:05:15 +0900

    update scripts

--- main.ts
+++ main.ts
@@ -1,44 +1,87 @@
 import packagejson = require("package-json");
 import { readFileSync, writeFileSync } from "fs";
 import fetch from "node-fetch";
+import { configure, getLogger } from "log4js";
+import dayjs from "dayjs";
 
 interface PkgData {
   versions: { [key: string]: packagejson.AbbreviatedVersion };
   name: string;
 }
+
 interface Data {
   name: string;
-  data: PkgData;
+  data: PkgData | object;
 }
+
+const logger = getLogger();
 
 const sleep: (number: number) => Promise<void> = msec => new Promise<void>(resolve => setTimeout(resolve, msec));
 
 const url = (name: string): string => `https://registry.npmjs.org/${encodeURIComponent(name)}`;
-const fetchPkg = async (name: string): Promise<Data> => {
+const fetchPkg = async (name: string, count = 0): Promise<Data> => {
   try {
     return await fetch(url(name))
       .then(v => v.json())
       .then(v => ({ name: name, data: v } as Data));
   } catch {
+    if (count > 5) {
+      logger.error(`error on request ${name} package 5 times`);
+      return { name, data: {} };
+    }
     await sleep(300);
-    console.log(`retry: ${name}`);
-    return fetchPkg(name);
+    return fetchPkg(name, count++);
   }
 };
 
 const convertData = (data: Data) => {
   try {
     const pkgData = data.data;
-    return {
-      name: pkgData.name,
-      versions: Array.from(Object.keys(pkgData.versions)).map(version => ({
-        version: version,
-        dep: pkgData.versions[version].dependencies
-      }))
-    };
+    if (pkgData["versions"] !== undefined && pkgData["versions"] !== null) {
+      const d = pkgData as PkgData;
+      return {
+        name: data.name,
+        versions: Array.from(Object.keys(d.versions)).map(version => ({
+          version: version,
+          dep: d.versions[version].dependencies
+        }))
+      };
+    } else {
+      return data.name;
+    }
   } catch {
     return data.name;
   }
+};
+
+const requests = (names: string[], interval = 5): Promise<Data>[] => {
+  const arr: Promise<Data>[] = [];
+  let sleeptime = 0;
+  let complete = 0;
+  for (let i = 0; i < names.length; i++) {
+    arr.push(
+      sleep(sleeptime)
+        .then(_ => fetchPkg(names[i]))
+        .then(v => {
+          complete += 1;
+          if (complete % 1000 == 0) logger.info(`end ${complete}/${names.length}`);
+          return v;
+        })
+    );
+    sleeptime += interval;
+  }
+  return arr;
+};
+
+const writeToFileAndReturnErrors = (e: Data[], index: number): string[] => {
+  const result = e.map(x => convertData(x));
+  const errors: string[] = result.filter<string>((v): v is string => typeof v == "string");
+  const ok = result.filter(v => typeof v != "string");
+  if (errors.length > 1) logger.error(`error on request ${errors.length} package: ${errors.join(",")}`);
+  logger.info(`ok: ${ok.length}, error: ${errors.length}`);
+  logger.info(`writing into file ${ok.length} packages`);
+  writeFileSync(`result/${index}.json`, JSON.stringify(ok));
+  return errors;
 };
 
 const MAX = 20000;
@@ -47,41 +90,18 @@ const main = async (): Promise<void> => {
   const start = process.argv.length > 2 ? Number(process.argv[2]) : 0;
   const names = JSON.parse(readFileSync("all-the-package-names/names.json").toString()) as string[];
   const separte = Math.floor(names.length / MAX);
-  console.log(`package count: ${names.length},file count: ${separte}`);
+  logger.info(`package count: ${names.length},file count: ${separte}`);
   for (let count = start; count <= separte; count++) {
-    console.log(`start ${count}/${separte}`);
     const start = count * MAX;
     const goal = (count + 1) * MAX - 1 > names.length ? names.length - 1 : (count + 1) * MAX - 1;
-    console.log(start, goal);
-    const arr: Promise<Data>[] = [];
-    let sleeptime = 0;
-    let complete = 0;
-    for (let i = start; i < goal; i++) {
-      arr.push(
-        sleep(sleeptime)
-          .then(_ => fetchPkg(names[i]))
-          .then(v => {
-            complete += 1;
-            if (complete % 1000 == 0) console.log(`end ${complete}/${goal - start}`);
-            return v;
-          })
-      );
-      sleeptime += 5;
-    }
-    console.log("start all promises...");
-    await Promise.all(arr)
-      .then(v => {
-        const result = v.map(x => convertData(x));
-        const errors = result.filter(v => typeof v == "string");
-        const ok = result.filter(v => typeof v != "string");
-        console.log(`error on request ${errors.length} package: ${errors.join(",")}`);
-        console.log("writing file...");
-        writeFileSync(`result/${count}.json`, JSON.stringify(ok));
-      })
-      .catch(v => {
-        console.log(`error: ${v}`);
-      });
+    logger.info(`start ${count}/${separte}: ${start} -> ${goal} (${goal - start})`);
+    const arr = requests(names.slice(start, goal));
+    await Promise.all(arr).then(v => writeToFileAndReturnErrors(v, count));
   }
 };
 
+configure({
+  appenders: { "get-all": { type: "file", filename: `logs/log_${dayjs().format("YYYYMMDD_HHmmss")}.log` } },
+  categories: { default: { appenders: ["get-all"], level: "info" } }
+});
 main();
