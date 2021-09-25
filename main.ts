dca7e09110fa03ce57700470a90a7166ffb77c79
sh4869 <nobuk4869@gmail.com>
2021-09-25 18:36:33 +0900

    色々修正

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 import packagejson = require("package-json");
-import { appendFile, appendFileSync, fstat, readFileSync, writeFileSync } from "fs";
-import fetch from "node-fetch";
-import { configure, getLogger } from "log4js";
 import dayjs from "dayjs";
+import { appendFileSync, readFileSync, writeFileSync } from "fs";
+import { promises } from "fs";
+import { configure, getLogger } from "log4js";
+import fetch from "node-fetch";
+import commandLineArgs, { OptionDefinition } from "command-line-args";
+import { sep } from "path";
 
 interface PkgData {
   versions: { [key: string]: packagejson.AbbreviatedVersion };
@@ -19,6 +22,7 @@ const logger = getLogger();
 const sleep: (number: number) => Promise<void> = msec => new Promise<void>(resolve => setTimeout(resolve, msec));
 
 const url = (name: string): string => `https://registry.npmjs.org/${encodeURIComponent(name)}`;
+
 const fetchPkg = async (name: string, count = 0): Promise<Data> => {
   while (count < 5) {
     try {
@@ -95,33 +99,62 @@ const requests = (names: string[], interval = 5): Promise<Data>[] => {
   return arr;
 };
 
-const writeToFileAndReturnErrors = (e: Data[], index: number): string[] => {
+const writeToFileAndReturnErrors = (e: Data[], index: number, dest: string, errorFile: string): string[] => {
   const result = e.map(x => convertData(x));
   const errors: string[] = result.filter<string>((v): v is string => typeof v == "string");
   const ok = result.filter(v => typeof v != "string");
   if (errors.length > 1) {
     logger.error(`error on request ${errors.length} package`);
-    appendFileSync("test.txt", errors.join("\n") + "\n");
+    appendFileSync(errorFile, errors.join("\n") + "\n");
   }
   logger.info(`ok: ${ok.length}, error: ${errors.length} (${errors.join(",")})`);
   logger.info(`writing into file ${ok.length} packages`);
-  writeFileSync(`result/${index}.json`, JSON.stringify(ok));
+  writeFileSync(`${dest}/${index}.json`, JSON.stringify(ok));
   return errors;
 };
 
-const MAX = 20000;
+const argOptions: OptionDefinition[] = [
+  { name: "name-file", defaultValue: "all-the-package-names/names.json", type: String },
+  { name: "error-file", defaultValue: "errorlist", type: String },
+  { name: "dest", defaultValue: "result", type: String },
+  { name: "retry-error", defaultValue: true, type: Boolean },
+  { name: "start", defaultValue: 0, type: Number },
+  { name: "max", defaultValue: 20000, type: Number }
+];
+
+const options = commandLineArgs(argOptions);
+
+const MAX = options["max"];
+
 const main = async (): Promise<void> => {
-  // 引数からファイル番号を受け取れるように
-  const start = process.argv.length > 2 ? Number(process.argv[2]) : 0;
-  const names = JSON.parse(readFileSync("all-the-package-names/names.json").toString()) as string[];
+  const start = options["start"];
+  const names = JSON.parse(readFileSync(options["name-file"]).toString()) as string[];
   const separte = Math.floor(names.length / MAX);
   logger.info(`package count: ${names.length},file count: ${separte}`);
   for (let count = start; count <= separte; count++) {
     const start = count * MAX;
-    const goal = (count + 1) * MAX - 1 > names.length ? names.length : (count + 1) * MAX;
+    const goal = Math.min(names.length, (count + 1) * MAX);
     logger.info(`start ${count}/${separte}: ${start} -> ${goal} (${goal - start})`);
-    const arr = requests(names.slice(start, goal), 10);
-    await Promise.all(arr).then(v => writeToFileAndReturnErrors(v, count));
+    const arr = requests(names.slice(start, goal), 5);
+    await Promise.all(arr).then(v => writeToFileAndReturnErrors(v, count, options["dest"], options["error-file"]));
+  }
+  // エラーをやるやつ
+  if (options["retry-error"]) {
+    const errorPackages = readFileSync(options["error-file"])
+      .toString()
+      .split("\n")
+      .filter((v, i, a) => a.indexOf(v) === i);
+    logger.info(`error package count: ${errorPackages.length}`);
+    const s = Math.floor(errorPackages.length / MAX);
+    for (let count = 0; count <= s; count++) {
+      const start = count * MAX;
+      const goal = Math.min(count + 1 * MAX, errorPackages.length);
+      logger.info(`start error ${count + separte + 1}/${s + separte + 1}: ${start} -> ${goal}`);
+      const arr = requests(errorPackages.slice(start, goal), 300);
+      await Promise.all(arr).then(v =>
+        writeToFileAndReturnErrors(v, count + separte + 1, options["dest"], options["error-file"])
+      );
+    }
   }
 };
 
